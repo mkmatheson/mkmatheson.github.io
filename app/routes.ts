fcf1b3da91e072e31ffed7bae1f3b49d4f3b9b65
mkmatheson <mckaymatheson@gmail.com>
2025-03-02 08:55:17 -0800

    revert routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,6 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
-  index('./chord-generator/src/App.tsx'),
-  route('chord-generator', 'routes/toe-touching.tsx')
-  //   index('routes/toe-touching.tsx'),
-  //   route('chord-generator', './chord-generator/src/App.tsx')
+  index('routes/toe-touching.tsx'),
+  route('chord-generator', './chord-generator/src/App.tsx')
 ] satisfies RouteConfig;
