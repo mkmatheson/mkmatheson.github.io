fed868426334ac278f3ad1eb07188a6c73bb0378
mkmatheson <mckaymatheson@gmail.com>
2025-03-01 19:07:29 -0800

    add toe-touching

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,17 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  ...{
+    rules: {
+      // suppress errors for missing 'import React' in files
+      "react/react-in-jsx-scope": "off",
+      // allow jsx syntax in js files (for next.js project)
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }], //should add ".ts" if typescript project
+    }
+  }
 ];
