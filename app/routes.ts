a807c965f181210fa1105458b21ece7b61fef166
mkmatheson <mckaymatheson@gmail.com>
2025-03-02 10:23:10 -0800

    move toe-touching code to its own repo

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
-  index('routes/toe-touching.tsx'),
+  index('./toe-touching/toe-touching.tsx'),
   route('chord-generator', './chord-generator/src/App.tsx')
 ] satisfies RouteConfig;
