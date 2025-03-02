782aed326a7e5b8655af1dad0a40ef941be811ab
mkmatheson <mckaymatheson@gmail.com>
2025-03-02 08:54:06 -0800

    temporarily reverse routes to check for proper page load

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
-  index('routes/toe-touching.tsx'),
-  route('chord-generator', './chord-generator/src/App.tsx')
+  index('./chord-generator/src/App.tsx'),
+  route('chord-generator', 'routes/toe-touching.tsx')
+  //   index('routes/toe-touching.tsx'),
+  //   route('chord-generator', './chord-generator/src/App.tsx')
 ] satisfies RouteConfig;
