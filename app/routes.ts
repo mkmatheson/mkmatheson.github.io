681ade2968cf2f0cda000616ed4b9a00b811d3c8
mkmatheson <mckaymatheson@gmail.com>
2025-11-02 20:34:58 -0800

    test changing how App.tsx is called

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
-  index('./App.tsx'),
+  index('App.tsx'),
   route('toe-touching', './toe-touching/toe-touching.tsx'),
   route('chord-generator', './chord-generator/src/App.tsx'),
-  route('guitar-note-guesser', './guitar-note-guesser/src/App.tsx'),
+  route('guitar-note-guesser', './guitar-note-guesser/src/App.tsx')
 ] satisfies RouteConfig;
