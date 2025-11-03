24f2382c8384816224e114abb596c7fa9893c65f
mkmatheson <mckaymatheson@gmail.com>
2025-11-02 20:39:16 -0800

    add proper export and revert routing

--- app/App.tsx
+++ app/App.tsx
@@ -14,3 +14,5 @@ const App = () => {
     </div>
   );
 };
+
+export default App;
--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
-  index('App.tsx'),
+  index('./App.tsx'),
   route('toe-touching', './toe-touching/toe-touching.tsx'),
   route('chord-generator', './chord-generator/src/App.tsx'),
   route('guitar-note-guesser', './guitar-note-guesser/src/App.tsx')
